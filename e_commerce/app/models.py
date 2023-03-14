7311ee51d669de42e56c8dea005492623fe8f4b5
somesh-py <someshsharma615@gmail.com>
2023-03-14 10:15:28 +0530

    create app and authenticate template files

---
+++ e_commerce/app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+STATE_CHOICES=(
+('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands'),
+('Andra Pradesh', 'Andra Pradesh'),
+('Arunachal Pradesh', 'Arunachal Pradesh'),
+('Assam', 'Assam'),
+('Bihar', 'Bihar'),
+('Chhattisgarh', 'Chhattisgarh'),
+('chandigarh', 'chandigarh'),
+('dadra & Nagar Haveli', 'dadra & Nagar Haveli'),
+('Delhi', 'Delhi'),
+('Madhya Pradesh', 'Madhya Pradesh'),
+('Utter Pradesh', 'Utter Pradesh'),
+('Andra Pradesh', 'Andra Pradesh'),
+('Mumbai', 'Mumbai'),
+('Mizoram', 'Mizoram'),
+('Nagaland', 'Nagaland'),
+)
+
